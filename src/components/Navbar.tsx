5cb083294313d9204808fcc4593ea3ce5f2f84bf
analourido <analouridofernandez4@gmail.com>
2025-02-03 11:43:26 +0100

    Cambios provisionales en Home

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <>
-            <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <nav className="bg-[rgba(255,255,255,0.8)]  dark:bg-[rgba(43,54,114,0.8)]  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 backdrop-blur-md transition-colors duration-300">
+
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="img\logo.png" className="h-8" alt="Bookify Logo" />
+                        <span className="self-center text-primary-60 font-semibold whitespace-nowrap dark:text-primary-90">Bookify</span>
                     </a>
                     <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                        <button type="button" className="text-primary-60 bg-primary-90 hover:bg-primary-85 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-primary-90 dark:hover:bg-primary-85 dark:focus:ring-blue-800">Get started</button>
                         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -20,21 +21,21 @@ function Navbar() {
                         </button>
                     </div>
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border-none rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0">
                             <li>
-                                <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
+                                <Link to="/" className="block py-2 px-3 text-primary-90 rounded-sm md:text-primary-90 md:p-0 md:dark:text-primary-90" aria-current="page">Home</Link>
                             </li>
                             <li>
-                                <Link to="/login" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</Link>
+                                <Link to="/login" className="block py-2 px-3 text-gray-900 rounded-sm hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white">Login</Link>
                             </li>
                             <li>
-                                <Link to="/register" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Registro</Link>
+                                <Link to="/register" className="block py-2 px-3 text-gray-900 rounded-sm hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white">Registro</Link>
                             </li>
                             <li>
-                                <Link to="/profile" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Perfil</Link>
+                                <Link to="/profile" className="block py-2 px-3 text-gray-900 rounded-sm hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white">Perfil</Link>
                             </li>
                             <li>
-                                <Link to="/userList" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Usuarios</Link>
+                                <Link to="/userList" className="block py-2 px-3 text-gray-900 rounded-sm hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white">Usuarios</Link>
                             </li>
                         </ul>
                     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>Home</div>
+        <>
+            <div className='m-5 p-5'>
+                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-primary-50 md:text-5xl lg:text-6xl dark:text-primary-50">Bienvenido a Bookify</h1>
+                <p className="text-lg font-normal text-primary-65 lg:text-xl dark:text-primary-65"> Descubre, recuerda y comparte tus historias favoritas.</p>
+                <button
+                    type="button"
+                    className="text-primary-50 bg-[rgba(247,229,221,0.81)]  focus:outline-none hover:bg-[rgba(242,203,180,0.9)] font-medium rounded-full text-sm px-5 py-2.5 my-5"
+                >
+                    Register here
+                </button>
+
+            </div>
+
+            <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl h-[172px] max-w-[301px] md:h-[294px] md:max-w-[512px]">
+                <div className="rounded-lg overflow-hidden h-[156px] md:h-[278px] bg-white dark:bg-gray-800">
+                    <img src="img\Diseño01.png" className="dark:hidden h-[156px] md:h-[278px] w-full rounded-lg" alt="" />
+                    <img src="img\Diseño01.png" className="hidden dark:block h-[156px] md:h-[278px] w-full rounded-lg" alt="" />
+                </div>
+            </div>
+            <div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm h-[17px] max-w-[351px] md:h-[21px] md:max-w-[597px]">
+                <div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[56px] h-[5px] md:w-[96px] md:h-[8px] bg-gray-800"></div>
+            </div>
+
+        </>
     )
 }
 
